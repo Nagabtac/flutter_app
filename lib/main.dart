869e177507e68e91e9e7f8405b2b058c0173dfc1
johnverz22 <johnny.verzola@lorma.edu>
2026-02-23 16:34:47 +0800

    syntaxes

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,60 @@ void main(){
     print(list[i]);
   }
 
+  list.forEach((item){
+    print(item);
+  });
+
   Map<String, String> likeColors= {"nagabtac":"red/blue/purple"};
   print(likeColors["nagabtac"]);
+
+  for(var colors in likeColors.values){
+    print(colors);
+  }
+
+  for(var key in likeColors.keys){
+    print("$key -> ${likeColors[key]}");
+  }
+
+
+  List<String> list2 =[...list,"x"];
+
+  print("this is list 2");
+  for(var l in list2){
+    print(l);
+  }
+
+  var area = circle_area(r: 5,pi :3.1416);
+  print("the area is $area");
+
+  print("creating object");
+  Student stud1 = Student(id:1, name: "nagabtac");
+  print(stud1.name);
+
+  var futureData = await fethData();
+  print(futureData);
 }
+  int sum(int num1, int num2){
+    return num1+num2;
+
+  }
+
+  int multiply(int a, int b) => a*b;
+
+  //[] positional parameter
+  //{} for named parameter
+  double circle_area({required double? r,double  pi = 3.14}){
+    return pi * r!* r;
+
+  }
+
+  class Student{
+    int id;
+    String name;
+
+    Student({required this.id, this.name});
+  }
+
+  Future<String> fetchData(){
+  await Future.delayed();
+  }
